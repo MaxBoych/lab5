b5937d7ace0723a5ee4c96270d3386f9107baf69
maxboychuk <maxboydad@gmail.com>
2019-11-28 22:31:20 +0300

    Scripted auto-commit on change (2019-11-28 22:31:20) by gitwatch.sh

--- src/main/java/СacheActor.java
+++ src/main/java/СacheActor.java
@@ -1,11 +1,16 @@
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class СacheActor extends AbstractActor {
 
-    private Map<String>
+    private Map<String, Integer> cache = new HashMap<>();
 
     @Override
     public Receive createReceive() {
-
+        return ReceiveBuilder.create()
+                .match()
     }
 }
