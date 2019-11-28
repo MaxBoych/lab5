e225cbdccebf21d02adf5aa87a5fcbd8eb2cdeb2
maxboychuk <maxboydad@gmail.com>
2019-11-28 22:04:06 +0300

    Scripted auto-commit on change (2019-11-28 22:04:06) by gitwatch.sh

---
+++ src/main/java/Config.java
@@ -0,0 +1,20 @@
+public class Config {
+
+    public static final String HOST = "localhost";
+    public static final int PORT = 8080;
+
+    public static final String SCRIPT_ENGINE_NAME = "nashorn";
+    public static final String PARAMETER_NAME = "packageId";
+    public static final int ACTORS_COUNT = 5;
+    public static final int TIMEOUT_MILLIS = 5000;
+
+    public static final String PACKAGE_ID = "packageId";
+    public static final String JS_SCRIPT = "jsScript";
+    public static final String FUNCTION_NAME = "functionName";
+    public static final String TESTS = "tests";
+    public static final String TEST_NAME = "testName";
+    public static final String EXPECTED_RESULT = "expectedResult";
+    public static final String PARAMS = "params";
+    public static final String RESULT = "result";
+    public static final String IS_EXPECTED = "isExpected";
+}
