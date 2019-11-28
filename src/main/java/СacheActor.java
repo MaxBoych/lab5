5716ae2fd7d1857e6a8eda0ecde7cf4c9f1b5d2b
maxboychuk <maxboydad@gmail.com>
2019-11-28 22:27:10 +0300

    Scripted auto-commit on change (2019-11-28 22:27:10) by gitwatch.sh

--- src/main/java/СacheActor.java
+++ src/main/java/СacheActor.java
@@ -2,8 +2,10 @@ import akka.actor.AbstractActor;
 
 public class СacheActor extends AbstractActor {
 
+    private Map<String >
+
     @Override
     public Receive createReceive() {
-        return null;
+
     }
 }
