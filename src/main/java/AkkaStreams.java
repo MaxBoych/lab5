77896aa7fde335f9bfc4d8d23b5a1c4900000978
maxboychuk <maxboydad@gmail.com>
2019-11-28 23:12:41 +0300

    Scripted auto-commit on change (2019-11-28 23:12:41) by gitwatch.sh

--- src/main/java/AkkaStreams.java
+++ src/main/java/AkkaStreams.java
@@ -1,4 +1,21 @@
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import org.asynchttpclient.AsyncHttpClient;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class AkkaStreams {
 
-    public 
+    public Flow<HttpRequest, HttpResponse, NotUsed> route(ActorRef cacheActor,
+                                                          AsyncHttpClient asyncHttpClient,
+                                                          ActorMaterializer materializer) {
+
+        return Flow.of(HttpRequest.class)
+                .map(request -> {
+
+                })
+    }
 }
